477bf52c04fa76a3f29399779003f3f78daa41bb
MrRobotS0 <guilhermebelinelo@outlook.com>
2025-05-02 19:23:12 -0300

    Arquivo de Conexão

--- backend/db.php
+++ backend/db.php
@@ -1,1 +1,18 @@
-echo "ola mundo"
+<?php
+$host = "localhost"; // Endereço do servidor
+$dbname = "durk"; // Nome do banco de dados
+
+try {
+    // Cria a conexão
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", null, null);
+    
+    // Define o modo de erro do PDO para exceções
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    // Mensagem de sucesso
+    echo "Conexão bem-sucedida!";
+} catch (PDOException $e) {
+    // Se houver erro na conexão, ele será capturado aqui
+    echo "Erro de conexão: " . $e->getMessage();
+}
+?>
