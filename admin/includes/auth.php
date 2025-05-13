2ad64f7d772064638f0a1e1ed8f0ca16dd753cd5
Guilherme Oliveira Belinelo <guilhermebelinelo@color.com.br>
2025-05-13 17:07:48 -0300

    update

--- admin/includes/auth.php
+++ admin/includes/auth.php
@@ -3,9 +3,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// Verifica se o usuário está logado e se é admin
 if (!isset($_SESSION['usuario_id']) || $_SESSION['tipo_usuario'] !== 'admin') {
-    // Redireciona para o site público (index da views)
     header('Location: ../../views/index.php');
     exit();
 }
